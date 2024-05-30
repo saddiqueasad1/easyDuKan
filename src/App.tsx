3579ad748bb2ee4fadf9b4de0507101deb1b9797
Abdul Aleem <engineerabdulaleem@gmail.com>
2024-05-30 15:15:41 +0500

    add  fetchProfile

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import i18n from "./locales/Translation";
 import { useState } from "react";
 import { I18nextProvider } from "react-i18next";
 import { app } from "./utills/firebaseConfig";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import { setProfile } from "./redux/slices/profilleSlice";
 
 const App = () => {
   const [hideSplashScreen] = useState(true);
+  const db = getFirestore();
+  const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
 
   const [fontsLoaded, error] = useFonts({
     "Outfit-Regular": require("./assets/fonts/Outfit-Regular.ttf"),
@@ -18,7 +25,24 @@ const App = () => {
 
   React.useEffect(() => {
     console.log(app.automaticDataCollectionEnabled);
-  }, []);
+
+    const fetchProfile = async () => {
+      try {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          const newData = { ...data, userId: user.uid };
+          dispatch(setProfile(newData as any));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (!!user.uid) fetchProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user.uid, db]);
 
   if (!fontsLoaded && !error) {
     return null;
